fb440cce7ef62b1e81d2eca3da478afcd8530adb
OceanLi <li437277219@gmail.com>
2023-12-18 10:55:02 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-videoFKSource_Tests/Pods-videoFKSource_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_videoFKSource_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_videoFKSource_Tests
+@end
---
+++ Example/Pods/Target Support Files/videoFKSource/videoFKSource-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_videoFKSource : NSObject
+@end
+@implementation PodsDummy_videoFKSource
+@end
